456c57475fe7a6108718424b830fa1d7a3c17564
RaulMG <raul.martinez.garrido@gmail.com>
2014-04-23 11:52:07 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,10 +7,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 	##Functions are defined to: 
 
-      get <- function() x            # get the initial matrix 
+      	get <- function() x            # get the initial matrix 
 	setInv<- function () solve(x)  # calculate the inverse matrix
 
-      list(get = get, setInv = setInv)    # Set the functions like atributes in a list asociated to x
+      	list(get = get, setInv = setInv)    # Set the functions like atributes in a list asociated to x
 }
 
 
@@ -18,16 +18,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 
-      ## Return a matrix that is the inverse of 'x'
+      	## Return a matrix that is the inverse of 'x'
 
 	matrix <- x$get()     # Loading the value of the matrix
 	dt <- det (matrix)    # Calculate the determinant of the matrix
 
-      if(is.null(dt)) {     # Check if the determinant is 0 and it's not possible to calculate the inverse matrix. 
-            message("The determinant is equal to 0. Matrix is not inversible.")
-            return()        # Exit the function with not solution.
-      }
+      	if(is.null(dt)) {     # Check if the determinant is 0 and it's not possible to calculate the inverse matrix. 
+            	message("The determinant is equal to 0. Matrix is not inversible.")
+            	return()        # Exit the function with not solution.
+      	}
 
-      invMtrx <- x$setInv() # Call the funcion to calcutate inverse the matrix x
-      return(invMtrx)       # Exit function with solution (Inverse Matrix)
+      	invMtrx <- x$setInv() # Call the funcion to calcutate inverse the matrix x
+      	return(invMtrx)       # Exit function with solution (Inverse Matrix)
 }
